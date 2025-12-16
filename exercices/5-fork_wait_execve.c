b20a81e66daecdd3f90d4fe8587c8db3d886e378
FelixBesancon <f.besancon@hotmail.fr>
2025-12-16 10:44:48 +0000

    exercices

---
+++ exercices/5-fork_wait_execve.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+int main(void)
+{
+	int count = 0;
+	pid_t PID;
+	char *args[] = {"ls", "-l", "/tmp", NULL};
+
+	PID = getpid();
+	printf("PPID is %u\n", PID);
+	while (count < 5)
+	{
+		PID  = fork();
+		if (PID == -1)
+		{
+			perror("fork");
+			return (-1);
+		}
+		if (PID == 0)
+		{
+			PID = getpid();
+			printf("PID is %u\n", PID);
+			execve("/bin/ls", args, NULL);
+			perror("execve");
+			return (-1);
+		}
+		else
+		{
+			wait(NULL);
+			PID = getpid();
+			printf("PPID is %u\n", PID);
+			count++;
+		}
+	}
+	return (0);
+}
